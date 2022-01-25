74e0c8684b966fb274fa31e2d892687f0d09001e
Tom Moss <thomas@thomasmoss.net>
2022-01-25 19:45:07 +0000

    Started working on Light/Dark mode setting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,38 @@
 import React from 'react'; 
 import './Header.css';
 import {NavLink} from 'react-router-dom'; 
+import Settings from './Settings';
 
 function Header() {
     return (
         /*making the header, we need to divide the header into three parts 
           each part will have items inside of it such as tabs, logo and so on
         */
-        <div className="header">
-            <div className="header__left">
+        <ul className="header">
+            <li className="header__left">
                <img src="/images/logo.PNG" alt=""></img>
                 <div className="SearchBar__input" >
                     <input placeholder="Search mashup" type="text"></input>
                 </div>
-            </div>
+            </li>
 
             {/*Here I'm declaring the bottons which will take us to other pages*/} 
-            <div className="header__middle">
+            <li className="header__middle">
                 <a href="Home" className="Header__item">Home</a>
                 <a href="MarketPlace" className="Header__item">Market Place</a>
                 <a href="DoodleCollab" className="Header__item">Doodle Collab</a>
-                <a href="Settings" className="Header__item">Settings</a> 
-            </div>
+            </li>
 
             {/*For the profile picture/username...still need to find out to have a picture*/} 
-            <div className="header__right">
+            <li className="header__right">
                 <div className="profile_image">
                     <h4>Magik</h4>
                 </div>
                 <a href="Login" className="Header__item">Login</a>
                 <a href="Signup" className="Header__item">Sign up</a>
-            </div>
-        </div>
+                <Settings />
+            </li>
+        </ul>
     )
 }
 
